c972dc9b758368e8b18a546cc42031acf9a46b60
ngocthieuff <ptnngoc22@gmail.com>
2022-09-22 14:24:18 +0700

    using pancakeswap ui package and add some styledcomponents

--- client/src/views/Swap/components/CurrencyInputHeader.tsx
+++ client/src/views/Swap/components/CurrencyInputHeader.tsx
@@ -1,3 +1,20 @@
+import styled from 'styled-components';
+import Flex from '../../../packages/uikit/src/components/Box/Flex';
+import IconButton from '../../../packages/uikit/src/components/Button/IconButton';
+
+const CurrencyInputContainer = styled(Flex)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 24px;
+    width: 100%;
+    border-bottom: 1px solid '#E7E3EB';
+`
+
+const ColoredIconButton = styled(IconButton)`
+    color: '#7A6EAA';
+`
+
 export const CurrencyInputHeader = () => {
     return (
         <p>CurrencyInputHeader</p>
